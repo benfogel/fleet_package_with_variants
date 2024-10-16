6bda7708c5a807f1a96e1df9fc443066245a6292
Ben Fogel <bfogel@google.com>
2024-10-16 22:01:50 +0000

    update

--- generate.sh
+++ generate.sh
@@ -3,7 +3,6 @@
 BASE_DIR="base"
 OVERLAYS_BASE="overlays" # Adjust if your base path is different
 PACKAGE_FOLDER="package"
-PACKAGE_PREFIX="pkg-"
 
 echo "Building all overlays"
 rm -rf "${PACKAGE_FOLDER}"
@@ -20,7 +19,7 @@ if [[ -d "$OVERLAYS_BASE" && -n "$(ls -A "$OVERLAYS_BASE")" ]]; then
 
         if [[ -f "$(pwd)/${OVERLAYS_BASE}/${overlay_name}/kustomization.yaml" ]]; then
             echo "Building overlay: $overlay_name" # Optional for progress updates
-            kustomize build ${OVERLAYS_BASE}/$overlay_name > ${PACKAGE_FOLDER}/${PACKAGE_PREFIX}$overlay_name.yaml
+            kustomize build ${OVERLAYS_BASE}/$overlay_name > ${PACKAGE_FOLDER}/$overlay_name.yaml
         else
             echo "No '${OVERLAYS_BASE}/${overlay_name}/kustomization.yaml' file found in overlay: $overlay_name. Skipping..."
             continue
@@ -29,5 +28,5 @@ if [[ -d "$OVERLAYS_BASE" && -n "$(ls -A "$OVERLAYS_BASE")" ]]; then
 
 else
     ## No overlays, build a global package from base
-    kustomize build "$(pwd)/$BASE_DIR" > ${PACKAGE_FOLDER}/pkg-global.yaml
+    kustomize build "$(pwd)/$BASE_DIR" > ${PACKAGE_FOLDER}/global.yaml
 fi
